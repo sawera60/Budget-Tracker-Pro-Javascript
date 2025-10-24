aaa2070a2698bf6c611387384a14897d801056fb
Sawera Sajid <sawerasajid60@gmail.com>
2025-10-24 16:44:13 +0500

    added css and js DOM selection

---
+++ script.js
@@ -0,0 +1,19 @@
+const popUp = document.querySelector(".pop-up");
+
+const incomeBox = document.querySelector(".income-box");
+const expenseBox = document.querySelector(".expense-box");
+const balanceBox = document.querySelector(".balance-box");
+
+const typeOptions = document.querySelector(".type-options");
+const typeIncomeBtn = document.getElementById("income");
+const typeExpenseBtn = document.getElementById("expense");
+const descriptionInput = document.querySelector(".description");
+const amountInput = document.querySelector("amount")
+const chooseCategory = document.getElementById("select-category");
+const addBtn = document.querySelector(".addbtn");
+
+
+
+const allTransactionDropdown = document.querySelector("select-transaction")
+const transactionCategory = document.getElementById(".select-category-transaction");
+const transactionDetails = document.querySelector(".lower")
